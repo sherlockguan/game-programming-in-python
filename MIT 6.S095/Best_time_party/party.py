405b2a37089507194bdae96e3a4c7fd3faaf2c5a
Sherlock Guan <46273832+sherlockguan@users.noreply.github.com>
2020-10-03 13:16:02 +0900

    Update party.py

--- MIT 6.S095/Best_time_party/party.py
+++ MIT 6.S095/Best_time_party/party.py
@@ -1,50 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 10:48:23 2020
 
-
+@author: guanfang
+"""
 # global variable
 sched = [(6, 8), (6, 12), (6, 7), (7, 8), (7, 10), (8, 9), (8, 10), (9, 12),
-            (9, 10), (10, 11), (10, 12), (11, 12)]
-################################################################################
-def bestTimeToParty(schedule):
+         (9, 10), (10, 11), (10, 12), (11, 12)]
+
+def bestTimeToParty(schedule): #Wrapper function
     start = schedule[0][0]
     end = schedule[0][1]
     for c in schedule:
-        start = min(c[0],start)
-        end = max(c[1],end)
-        #print('starting time is',start) 6
-        #print('end time is',end) 12
+        start = min(start,c[0])
+        end = max(start, c[1])
     
-    #compute count of celebrities at each time
-    count = celebrityDensity(schedule, start, end)
-    #print(count)
-    
-    maxcount = 0
-    for i in range(start, end + 1): # i = 6,7,8,9,..,12
-        if count[i] > maxcount: # eg. count[6] the num of celebrity at 6 o'clock
-            maxcount = count[i]
-            time = i # think about why we can not print the i as time directly  i final get
-                    #update as 12
-                     #time keep track of the timing when maximum number of celebrity around
-            
-    print('Best time to attend the party is at', time,\
-          'o\'clock',':',maxcount,'celebrities will be attending!')
+    count = count_celebrity(schedule,start, end)
+    max_count = 0
+    for i in range(len(count)):
+        if count[i] > max_count:
+            max_count = count[i]
+            time = i
         
+   
     
-################################################################################
+    
+    print('best time to attend a party is',time," o'clock",max_count,
+    'celebrities will be attending')
+    
 
-  #compute count of celebrities at each time
-def celebrityDensity(schedule, start, end):
-    #print(start)
-    #print(end)
-    count = [0]*(end + 1)
-    # count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for i in range(start, end + 1): # i = 6,7,8...12
-        count[i] = 0
-        for c in sched:
-            if c[0] <= i and c[1] > i:
-                count[i] += 1
-                
-    return count
+# # for each time, count arrivals and record the responding time i as time_variable
+def count_celebrity(schedule,start,end): # write THIS FUNC FIRST
+    count = [0]*(end + 1)  # 13 is "magic number" fewer code but few variable may cause ambiguousity
     
-################################################################################                
-# the  entrance (main) function
-bestTimeToParty(sched)  
+    for i in range(start, end + 1):
+        count[i] = 0
+        for c in schedule:
+            if c[0] <= i and c[1]> i:
+                count[i]+= 1
+
+    return count # test it seperately
+    #count_celebrity(sched)
+
+    #[0, 0, 0, 0, 0, 0, 3, 4, 4, 5, 4, 4, 0]
+
+bestTimeToParty(sched)
+
+
+
+
+
+
+    
+    
+        
